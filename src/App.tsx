e1808e638ed2ee7cc2b2efe8a13a57f9a6dd6c05
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2025-01-03 04:51:21 +0300

    add elevation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, CssBaseline, Typography } from "@mui/material";
+import { Container, CssBaseline, Typography, Paper } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AuditLogTable from "./components/AuditLogTable";
 
@@ -14,14 +14,16 @@ function App() {
 		<>
 			<ThemeProvider theme={defaultTheme}>
 				<CssBaseline />
-				<Container maxWidth="lg" sx={{ pt: 5 }}>
-					<Typography variant="h4" color="primary" gutterBottom>
-						Audit Logs
-					</Typography>
-					<Typography variant="subtitle1" color="textSecondary" gutterBottom>
-						View detailed logs of actions performed in the bank application.
-					</Typography>
-					<AuditLogTable />
+				<Container maxWidth="lg" sx={{ pt: 3 }}>
+					<Paper elevation={10} sx={{ padding: 3 }}>
+						<Typography variant="h4" color="primary" gutterBottom>
+							Audit Logs
+						</Typography>
+						<Typography variant="subtitle1" color="textSecondary" gutterBottom>
+							View detailed logs of actions performed in the bank application.
+						</Typography>
+						<AuditLogTable />
+					</Paper>
 				</Container>
 			</ThemeProvider>
 		</>
